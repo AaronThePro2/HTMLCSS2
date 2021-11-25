6e48301da3de2037489087586f1587ca240392b1
Aaronthepro2.0 <Aaron.Coltof@coderclass.nl>
2021-11-25 10:14:35 +0100

    Badge unity-1 toegevoegd

--- SCRIPTS/Badges-script.js
+++ SCRIPTS/Badges-script.js
@@ -1,4 +1,4 @@
-const badges = ["Vrije keuzemodule level-4", "HTML-CSS-2", "CSS-games", "Linux-0", "Level 4", "Datarepresentatie-0", "Minecraft in python-0", "Unity-0", "Python-2", "Joker-3", "JavaScript voor web-0", "Python-1B", "Vrijekeuze module level 3", "SQL-0", "HTML-CSS-1", "Hardware-0", "Level 3", "Blender-0", "Vrije keuzemodule level 2", "Blokjes programmeren-1", "Python-1A", "Embedded-0", "Level 2", "Python-0", "HTML-CSS-0", "Coderclass omgeving-0", "Blokjesprogrammeren-0", "Level-1"];
+const badges = ["Unity-1","Vrije keuzemodule level-4", "HTML-CSS-2", "CSS-games", "Linux-0", "Level 4", "Datarepresentatie-0", "Minecraft in python-0", "Unity-0", "Python-2", "Joker-3", "JavaScript voor web-0", "Python-1B", "Vrijekeuze module level 3", "SQL-0", "HTML-CSS-1", "Hardware-0", "Level 3", "Blender-0", "Vrije keuzemodule level 2", "Blokjes programmeren-1", "Python-1A", "Embedded-0", "Level 2", "Python-0", "HTML-CSS-0", "Coderclass omgeving-0", "Blokjesprogrammeren-0", "Level-1"];
 let aantalbadges = badges.length;
 let text1 = document.getElementById("Aantalbadgesbehaald");
 let list = document.getElementById("list");
